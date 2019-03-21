7b97e842da03f2d27cd77757ab82a2d62433eaf7
Daniel Bok <daniel.bok@outlook.com>
2019-03-21 20:19:37 +0800

    Fix bug Travis PyPI

--- setup.py
+++ setup.py
@@ -40,8 +40,7 @@ def run_setup():
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Topic :: Financial/Engineering',
+            'Programming Language :: Python :: 3.7'
         ],
         install_requires=install_requires,
         python_requires='>=3.6',
