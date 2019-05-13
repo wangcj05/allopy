6886816941e0e9fa65c7953547f87bf7d5903c33
Daniel Bok <daniel.bok@outlook.com>
2019-05-13 10:10:48 +0800

    Refactored ASROptimizer module
    Removed _Funcs. Simplified inner workings of the optimizer

--- allopy/optimize/asr.py
+++ allopy/optimize/asr.py
@@ -7,6 +7,8 @@ from .algorithms import LD_SLSQP
 from .base import BaseOptimizer
 from .opt_data import OptData
 
+__all__ = ['ASROptimizer']
+
 EPSILON = np.finfo('float').eps ** (1 / 3)
 OptArray = Optional[Array]
 Real = Union[int, float]  # a real number
@@ -65,29 +67,28 @@ class ASROptimizer(BaseOptimizer):
         self.data = data
         self.cvar_data = cvar_data
 
-        self._funcs = _Functions(data, cvar_data, rebalance)
-        self.AP = _APObjectives(self, self._funcs)
+        self.AP = APObjectives(self)
+        self._rebalance = rebalance
 
     def adjust_returns(self, eva: Optional[Iterable[float]] = None, vol: Optional[Iterable[float]] = None):
-        self._funcs.adjust_returns(eva, vol)
+        self.data = self.data.calibrate_data(eva, vol)
         return self
 
     def adjust_cvar_returns(self, eva: Optional[Iterable[float]] = None, vol: Optional[Iterable[float]] = None):
-        self._funcs.adjust_cvar_returns(eva, vol)
+        self.cvar_data = self.cvar_data.calibrate_data(eva, vol)
         return self
 
     @property
     def rebalance(self):
-        return self._funcs.rebalance
+        return self._rebalance
 
     @rebalance.setter
     def rebalance(self, rebal: bool):
         assert isinstance(rebal, bool), 'rebalance parameter must be boolean'
+        self._rebalance = rebal
 
-        self._funcs.rebalance = rebal
 
-
-class _APObjectives:
+class APObjectives:
     """
     Active objectives. Active is used when the returns stream of the simulation is the over (under) performance of
     the particular asset class over the benchmark. (The first index in the assets axis)
@@ -95,11 +96,12 @@ class _APObjectives:
     For example, if you have a benchmark (beta) returns stream, 9 other asset classes over 10000 trials and 40 periods,
     the simulation tensor will be 40 x 10000 x 10 with the first asset axis being the returns of the benchmark. In
     such a case, the active portfolio optimizer can be used to optimize the portfolio relative to the benchmark.
+
+    This is a singleton class meant for easier optimization regime access for the ASROptimizer
     """
 
-    def __init__(self, asr: ASROptimizer, funcs: '_Functions'):
+    def __init__(self, asr: ASROptimizer):
         self.asr = asr
-        self.funcs = funcs
 
     def max_eva_st_risk(self, max_te: OptReal = None, max_cvar: OptReal = None, x0: OptArray = None):
         """
@@ -126,18 +128,18 @@ class _APObjectives:
         ndarray
             Optimal weights
         """
-        opt, f = self.asr, self.funcs
+        opt = self.asr
 
         assert not (max_te is None and max_cvar is None), "If maximizing EVA subject to some sort of TE/CVaR " \
                                                           "constraint, we must at least specify max CVaR or max TE"
 
         if max_te is not None:
-            opt.add_inequality_constraint(f.tracking_error_cons(max_te))
+            opt.add_inequality_constraint(tracking_error_constraint(opt.data, max_te))
 
         if max_cvar is not None:
-            opt.add_inequality_constraint(f.cvar_cons(max_cvar))
+            opt.add_inequality_constraint(cvar_cons(opt.data, max_cvar, opt.rebalance))
 
-        opt.set_max_objective(f.expected_returns_obj)
+        opt.set_max_objective(expected_returns_obj(opt.data, opt.rebalance))
         return opt.optimize(x0)
 
     def min_tracking_error(self, min_ret: OptReal = None, use_active_return=False, x0: OptArray = None):
@@ -164,13 +166,12 @@ class _APObjectives:
         ndarray
             Optimal weights
         """
-
-        opt, f = self.asr, self.funcs
+        opt = self.asr
 
         if min_ret is not None:
-            opt.add_inequality_constraint(f.expected_returns_cons(min_ret, use_active_return))
+            opt.add_inequality_constraint(expected_returns_cons(opt.data, min_ret, use_active_return, opt.rebalance))
 
-        opt.set_min_objective(f.tracking_error_obj)
+        opt.set_min_objective(tracking_error_obj(opt.data))
         return opt.optimize(x0)
 
     def min_cvar(self, min_ret: OptReal = None, use_active_return=False, x0: OptArray = None):
@@ -198,12 +199,12 @@ class _APObjectives:
         ndarray
             Optimal weights
         """
-        opt, f = self.asr, self.funcs
+        opt = self.asr
 
         if min_ret is not None:
-            opt.add_inequality_constraint(f.expected_returns_cons(min_ret, use_active_return))
+            opt.add_inequality_constraint(expected_returns_cons(opt.data, min_ret, use_active_return, opt.rebalance))
 
-        opt.set_min_objective(f.cvar_obj)
+        opt.set_min_objective(cvar_obj(opt.data, opt.rebalance))
         return opt.optimize(x0)
 
     def max_info_ratio(self, x0: OptArray = None):
@@ -215,105 +216,71 @@ class _APObjectives:
         :return: ndarray
             optimal weights
         """
-
-        opt, f = self.asr, self.funcs
-        opt.set_max_objective(f.info_ratio_obj)
+        opt = self.asr
+        opt.set_max_objective(info_ratio_obj(opt.data, opt.rebalance))
         return opt.optimize(x0)
 
 
-def _to_scalar_(name: str):
-    """Convenience decorator to convert numbers to floats and raise error if a number is not passed in"""
+def cvar_cons(data: OptData, max_cvar: Real, rebalance=False):
+    """
+    Creates a cvar constraint function.
 
-    def wrapper(func):
-        def decorator(cls, value, *args, **kwargs):
-            assert np.isscalar(value), f'{name} must be a numeric scalar'
+    By default, the constraint function signature is :code:`CVaR(x) >= max_cvar` where `max_cvar`
+    is a negative number. Meaning if you would like to cap cvar at -40%, max_cvar should be set to -0.4.
+    """
 
-            value = float(value)
-            return func(cls, value, *args, **kwargs)
+    def cvar(w):
+        return float(max_cvar) - data.cvar(w, rebalance)
 
-        return decorator
-
-    return wrapper
+    return cvar
 
 
-class _Functions:
-    def __init__(self, data: OptData, cvar_data: OptData, rebalance: bool):
-        self.data = data
-        self.cvar_data = cvar_data
-        self._rebal = rebalance
+def cvar_obj(data: OptData, rebalance=False):
+    # cvar works in negatives. So to reduce cvar, we have to "maximize" it
 
-    def adjust_returns(self, eva, vol):
-        self.data.calibrate_data(eva, vol, inplace=True)
+    def cvar(w):
+        return -data.cvar(w, rebalance)
 
-    def adjust_cvar_returns(self, eva, vol):
-        self.cvar_data = self.cvar_data.data.calibrate_data(eva, vol, inplace=True)
+    return cvar
 
-    @_to_scalar_('max_cvar')
-    def cvar_cons(self, max_cvar: Real):
-        """
-         Creates a cvar constraint function.
 
-        By default, the constraint function signature is :code:`CVaR(x) >= max_cvar` where `max_cvar` 
-        is a negative number. Meaning if you would like to cap cvar at -40%, max_cvar should be set to -0.4.
-        
-        Parameters
-        ----------
-        max_cvar: {int, float}
-            Maximum cvar.
-        """
+def expected_returns_cons(data: OptData, min_ret: Real, use_active_return: bool, rebalance=False):
+    exp_ret = expected_returns_obj(data, rebalance)
 
-        def cvar(w):
-            return max_cvar - self.data.cvar(w, self.rebalance)
+    def exp_ret_con(w):
+        _w = [0, *w[1:]] if use_active_return else w
+        # usually the constraint is >= min_ret, thus need to flip the sign
+        return float(min_ret) - exp_ret(w)
 
-        return cvar
+    return exp_ret_con
 
-    def cvar_obj(self, w):
-        # cvar works in negatives. So to reduce cvar, we have to "maximize" it
-        return -self.data.cvar(w, self.rebalance)
 
-    @_to_scalar_('min_ret')
-    def expected_returns_cons(self, min_ret: Real, use_active_return: bool):
-        def exp_ret(w):
-            _w = [0, *w[1:]] if use_active_return else w
-            # usually the constraint is >= min_ret, thus need to flip the sign
-            return min_ret - self.expected_returns_obj(_w)
+def expected_returns_obj(data: OptData, rebalance=False):
+    def exp_ret(w):
+        return data.expected_return(w, rebalance)
 
-        return exp_ret
+    return exp_ret
 
-    def expected_returns_obj(self, w):
-        return self.data.expected_return(w, self.rebalance)
 
-    def info_ratio_obj(self, w):
+def info_ratio_obj(data: OptData, rebalance=False):
+    def info_ratio(w):
         _w = [0, *w[1:]]
-        return self.data.sharpe_ratio(_w, self.rebalance)
+        return data.sharpe_ratio(_w, rebalance)
 
-    @_to_scalar_('max_te')
-    def tracking_error_cons(self, max_te: Real):
-        """
-        Creates a tracking error function.
+    return info_ratio
 
-        By default, the constraint function signature is :code:`TE(x) <= max_te`. Meaning if you would like to 
-        cap tracking error to 3%, max_te should be set to 0.03.
-        
-        Parameters
-        ----------
-        max_te: {int, float}
-            Maximum tracking error
-        """
 
-        def te(w):
-            return self.tracking_error_obj(w) - max_te
+def tracking_error_constraint(data: OptData, max_te: Real):
+    te_obj = tracking_error_obj(data)
 
-        return te
+    def te(w):
+        return te_obj(w) - float(max_te)
 
-    def tracking_error_obj(self, w):
-        _w = [0, *w[1:]]
-        return self.data.volatility(_w)
+    return te
 
-    @property
-    def rebalance(self):
-        return self._rebal
 
-    @rebalance.setter
-    def rebalance(self, rebal: bool):
-        self._rebal = rebal
+def tracking_error_obj(data: OptData):
+    def te(w):
+        return data.volatility([0, *w[1:]])
+
+    return te
