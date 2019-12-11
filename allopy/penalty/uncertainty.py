37e28a99bb37d0fb62f0f5625bfdddb0746fdbf8
Daniel Bok <daniel.bok@outlook.com>
2019-12-11 13:32:50 +0800

    Add chi2 default parameter to UncertaintyPenalty

--- allopy/penalty/uncertainty.py
+++ allopy/penalty/uncertainty.py
@@ -1,12 +1,15 @@
 from typing import Iterable, Union
 
 import numpy as np
+from scipy.stats import chi2
 
 from .abstract import Penalty
 
 
 class UncertaintyPenalty(Penalty):
-    def __init__(self, uncertainty: Union[Iterable[Union[int, float]], np.ndarray], lambda_=1):
+    def __init__(self, uncertainty: Union[Iterable[Union[int, float]], np.ndarray],
+                 lambda_: float = 0.95,
+                 method='direct'):
         r"""
         The uncertainty penalty. It penalizes the objective function relative to the level of uncertainty for the
         given asset
@@ -21,6 +24,13 @@ class UncertaintyPenalty(Penalty):
         where :math:`\Phi` represent the uncertainty matrix. :math:`\lambda = 0` or a 0-matrix is a special case
         where there are no uncertainty in the projections.
 
+        If using :math:`\chi^2` method, the :math:`\lambda` value is given by
+
+        .. math::
+            \lambda = \frac{1}{\chi^2_{n - 1}(\alpha)}
+
+        where :math:`n` is the number of asset classes and :math:`\alpha` is the confidence interval
+
         Parameters
         ----------
         uncertainty:
@@ -29,18 +39,40 @@ class UncertaintyPenalty(Penalty):
 
         lambda_:
             A constant controlling the intensity of the penalty
+
+        method: "chi2" or "direct"
+            Method used to construct the lambda parameter. If "direct", the exact value specified by the lambda_
+            parameter is used. If "chi2", the value is determined using the inverse of the chi-square quantile
+            function. In that instance, the lambda_ parameter will be the confidence level. See Notes.
         """
-
-        self._lambda = lambda_
-        self._uncertainty = np.asarray(uncertainty)
-        if self._uncertainty.ndim == 1:
-            self._uncertainty = np.diag(self._uncertainty)
-
+        self._uncertainty = self._derive_uncertainty(np.asarray(uncertainty))
         self.dim = len(self._uncertainty)
-        assert self._uncertainty.ndim == 2, ""
+        self._method = method.lower()
+        self._lambda = self._derive_lambda(lambda_, self._method, self.dim)
 
     def cost(self, w: np.ndarray) -> float:
         return self._lambda * (w @ self._uncertainty @ w) ** 0.5
+
+    @property
+    def uncertainty(self):
+        return self._uncertainty
+
+    @staticmethod
+    def _derive_lambda(value: float, method: str, dim: int):
+        assert method in ('chi2', 'direct'), f"Unknown method: {method}. Use 'chi2' or 'direct'"
+        if method == "direct":
+            return value
+        else:
+            assert 0 <= value <= 1, "lambda_ (alpha) parameter must be between 0 and 1 (inclusive) if using 'chi2'"
+            return chi2.ppf(value, dim - 1)
+
+    @staticmethod
+    def _derive_uncertainty(uncertainty: np.ndarray):
+        if uncertainty.ndim == 1:
+            uncertainty = np.diag(uncertainty)
+
+        assert uncertainty.ndim == 2, "uncertainty input must be 1 or 2 dimensional"
+        return uncertainty
 
     def __str__(self):
         arr = repr(self._uncertainty.round(4)).replace("array(", "").replace(")", "")
@@ -48,6 +80,7 @@ class UncertaintyPenalty(Penalty):
         return f"""
 UncertaintyPenalty(
     lambda={self._lambda},
-    uncertainty={arr}
+    uncertainty={arr},
+    method={self._method}
 )        
         """.strip()
